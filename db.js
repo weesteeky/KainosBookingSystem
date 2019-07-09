f08f71853c6738f8a71724923ce83b2a790b2eaa
BenAAndrew <bandrew01@qub.ac.uk>
2019-07-09 12:20:11 +0100

    database queries

--- db.js
+++ db.js
@@ -7,19 +7,21 @@ var con = mysql.createConnection({
     database: 'BookingSystem'
 });
 
-function executeQuery(sql){
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        con.query(sql, function (err, result, fields) {
-          if (err) throw err;
-          console.log(sql+" log successful")
-          console.log(result) 
-        });
-    });
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function query(sql, args){
+    return new Promise( ( resolve, reject ) => {
+        con.query( sql, args, ( err, rows ) => {
+            if ( err )
+                return reject( err );
+            resolve( rows );
+        } );
+    } );
 }
 
-results = executeQuery("SELECT * FROM Course")
-/*for(var i = 0; i < results.length; i++){
-    console.log(results[i])
-}*/
+query( 'SELECT * FROM Course' ).then( rows => {
+    console.log(rows)
+} );
