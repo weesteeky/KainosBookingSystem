b0d8e45294ad5bd361d1c3bebbf85b01b1c72b66
BenAAndrew <bandrew01@qub.ac.uk>
2019-07-10 09:50:50 +0100

    cors support

--- express.js
+++ express.js
@@ -1,4 +1,5 @@
 const Database = require('./db.js')
+const cors = require('cors')
 const express = require('express');
 	
 const app = express();
@@ -46,7 +47,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 	
 //Access the parse results as request.body
-app.post('/addcourse', function(request, response){
+app.post('/addcourse', cors(), function(request, response){
 	console.log(request.body);
 	var ans = addCourse(request.body);
 
@@ -56,6 +57,12 @@ app.post('/addcourse', function(request, response){
 	else{
 		response.send("Failed to add new Course");
 	}
+});
+
+app.get('/courses', cors(), function(request, response){
+    db.query("SELECT * FROM Course").then(rows => {
+        response.send(rows)
+    })
 });
 	
 app.listen(PORT,() => {
